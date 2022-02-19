a258f7f79ed753f10ebd1e7c81b100e8e84ae93b
albe2669 <albert@risenielsen.dk>
2022-02-20 00:39:57 +0100

    Added requiresAuth to router.beforeEach

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import { useMessageStore } from '@/stores/message';
+import { useUserStore } from '@/composables/useUserStore';
+
 import Login from '@/views/Login.vue';
 import Register from '@/views/Register.vue';
 
 import PublicTimeline from '@/views/PublicTimeline.vue';
-import { useMessageStore } from '@/stores/message';
+import MyTimeline from '@/views/MyTimeline.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    /*{
+    {
       path: '/',
       name: 'my timeline',
-      component: MyTimeline
-    },*/
+      component: MyTimeline,
+      meta: {
+        requiresAuth: true,
+        authRedirect: '/public'
+      }
+    },
     {
       path: '/public',
       name: 'public timeline',
@@ -40,6 +48,15 @@ router.beforeEach((to, from) => {
   const { clearErrors } = useMessageStore();
 
   clearErrors();
+
+  if (to.meta.requiresAuth) {
+    const { isLoggedIn } = useUserStore();
+    console.log(to.meta.authRedirect as string, isLoggedIn.value);
+
+    if (!isLoggedIn.value) {
+      return { path: to.meta.authRedirect as string || '/login' };
+    }
+  }
 });
 
 export default router
