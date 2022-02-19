1e8d44a90fd4b90cecc3fa9b6303e88618899dcd
albe2669 <albert@risenielsen.dk>
2022-02-19 19:49:31 +0100

    Add promise.prototype.finally shim to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { shim } from 'promise.prototype.finally';
+shim();
+
 import App from './App.vue'
 import router from './router'
 
