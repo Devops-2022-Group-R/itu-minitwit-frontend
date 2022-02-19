fb6d3ef475d207e389525983a6be49988074d99d
albe2669 <albert@risenielsen.dk>
2022-02-20 00:54:59 +0100

    Create MyTimeline

---
+++ src/views/MyTimeline.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useMessages } from "@/composables/useMessages";
+import { useUserStore } from "@/composables/useUserStore";
+import { createAuthHeader } from "@/utils/http-client";
+import { ref } from "vue";
+
+import LoadingWrapper from "../components/LoadingWrapper.vue";
+import Timeline from "../components/Timeline.vue";
+
+const { hash } = useUserStore();
+
+const loading = ref(false);
+
+const { messages } = useMessages("/feed", loading, {
+    headers: {
+        ...createAuthHeader(hash.value)
+    }
+});
+</script>
+
+<template>
+    <Timeline :messages="messages" title="My Timeline" :loading="loading">
+        <template #followstatus>This is you!</template>
+    </Timeline>
+</template>
