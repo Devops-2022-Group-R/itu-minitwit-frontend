6a41c6063cdc05509763d582eb52b397e865c686
albe2669 <albert@risenielsen.dk>
2022-02-20 01:36:47 +0100

    Fixed timelinemessage user url

--- src/components/TimelineMessage.vue
+++ src/components/TimelineMessage.vue
@@ -9,7 +9,7 @@ const props = defineProps<{
 
 const username = computed(() => props.message.Author.Username);
 const avatar = computed(() => getGravatarUrl(props.message.Author.Email, 48));
-const userUrl = computed(() => `/user/${username.value}`);
+const userUrl = computed(() => `/feed/${username.value}`);
 const pubDate = computed(() => formatDateTime(props.message.PubDate));
 </script>
 
@@ -18,7 +18,7 @@ const pubDate = computed(() => formatDateTime(props.message.PubDate));
         <img :src="avatar" />
         <p>
             <strong>
-                <a :href="userUrl">{{ username }}</a>
+                <router-link :to="userUrl">{{ username }}</router-link>
             </strong>
             {{ message.Text }}
             <small>&mdash; {{ pubDate }}</small>
